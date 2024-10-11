a5adb50fe58ef07e12d04276d66a0e3c636f4441
xilinl3 <xilinl3@uci.edu>
2024-10-11 16:28:23 -0700

    PlayerAnimation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,27 +4,74 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float moveSpeed = 5f;  // 角色的移动速度
-
+    //Component
     private Rigidbody2D rb;
-    private float moveInput;
+    private Animator anim;
+
+    //PlayerADMove
+    [SerializeField] private float moveSpeed = 5f; // 角色的移动速度
+    private float xInput;
+    public bool IsRight = true;
+    private int facingDir = 1;
 
     void Start()
     {
         // 获取角色的 Rigidbody2D 组件
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         // 获取水平输入 (A, D 键或左、右箭头键)
-        moveInput = Input.GetAxis("Horizontal");
+        xInput  = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        HandleAnimation();
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") < 0 || Input.GetAxis("Horizontal") > 0)
+        {
+            TurnCheck();
+        }
     }
 
-    void FixedUpdate()
+    private void HandleAnimation()
     {
-        // 应用水平移动
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        anim.SetFloat("xVelocity", rb.velocity.x);
+    }
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        transform.Rotate(0, 180, 0);
+    }
+    private void TurnCheck()
+    {
+        if (Input.GetAxis("Horizontal") > 0 && !IsRight || Input.GetAxis("Horizontal") < 0 && IsRight)
+        {
+            Flip();
+            Trun();
+        }
+    }
+
+    public void Trun()
+    {
+        if (IsRight)
+        {
+            //Vector3 rotator = new Vector3(transform.rotation.x, 180f, transform.rotation.z);
+            //transform.rotation = Quaternion.Euler(rotator);
+            IsRight = !IsRight;
+
+            //tirn the camera follow object
+            //_cameraFollowObject.CallTurn();
+        }
+        else
+        {
+            //Vector3 rotator = new Vector3(transform.rotation.x, 0f, transform.rotation.z);
+            //transform.rotation = Quaternion.Euler(rotator);
+            IsRight = !IsRight;
+
+            //_cameraFollowObject.CallTurn();
+        }
     }
 }

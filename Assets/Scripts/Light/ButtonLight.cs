b0f53fa60152a6fef164c3a93634fc6f0b36a7c2
xilinl3 <xilinl3@uci.edu>
2024-10-18 15:39:36 -0700

    buttonlight

---
+++ Assets/Scripts/Light/ButtonLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLight : MonoBehaviour
+{
+    public Color32 redColor = new Color32(255, 0, 0, 255);    // 红色
+    public Color32 greenColor = new Color32(0, 255, 0, 255);  // 绿色
+    public Color32 blueColor = new Color32(0, 0, 255, 255);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

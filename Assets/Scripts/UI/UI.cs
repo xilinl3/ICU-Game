9e9890881a421bd26fdfc4835d4a35f2adc42355
ShaCoZha <jerry.zhang0825@gmail.com>
2024-10-27 20:02:39 -0700

    初步-inputmanager

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Runtime.InteropServices;
 
 public class UI : MonoBehaviour
 {
     [SerializeField]public float amendment = 1f;
-   //开始键
-   public void Starting()
+
+    [DllImport("User32.dll", EntryPoint = "keybd_event")]
+    static extern void keybd_event(byte bvk, byte bScan, int dwFlags, int dwExtraInfo);
+
+    //开始键
+    public void Starting()
     {
         SceneManager.LoadScene(1);
         Time.timeScale = 1;
@@ -93,6 +98,32 @@ public class UI : MonoBehaviour
         GameObject pauseMenu = GameObject.Find("Canvas").transform.Find("StopPage").gameObject;
         amendment = 0f;
         pauseMenu.SetActive(true);
+    }
+
+    public void pressA()
+    {
+        keybd_event(65, 0, 1, 0);
+    }
+
+    public void pressSpace()
+    {
+        keybd_event(32, 0, 0, 0);
+    }
+
+    public void pressD()
+    {
+        Debug.Log("D has been pressed.");
+        keybd_event(68, 0, 1, 0);
+    }
+
+    public void pressF()
+    {
+        keybd_event(70, 0, 0, 0);
+    }
+
+    public void pressR()
+    {
+        keybd_event(82, 0, 0, 0);
     }
 }
 

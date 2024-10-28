c32de031e496f60f6e35517098ac1d6259e7bc40
xilinl3 <xilinl3@uci.edu>
2024-10-27 17:17:33 -0700

    玩家可以跟随移动平台

---
+++ Assets/Scripts/Item/MPlatformControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MPlatformControl : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 检查是否碰撞到带有 "Player" 标签的对象
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // 将玩家的父对象设置为当前移动平台
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // 当玩家离开平台时，取消父子关系
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
